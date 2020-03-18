4a08ec8a5754222f5d71597a9bf6d4c6fb557699
ShantelAcker <56419079+shantelacker@users.noreply.github.com>
2020-03-18 10:49:30 -0500

    create theme.js

---
+++ theme.js
@@ -0,0 +1,12 @@
+var selectTheme = document.getElementById("theme");
+
+if (selectTheme.value = "original")
+{
+    document.body.style.backgroundColor = "#03031a";
+    document.body.style.color =" #708090";
+} else if (selectTheme.value = "landscape")
+{
+    document.body.style.background = "url('images/forest.jpg')";
+    document.body.style.color = white;
+
+}
